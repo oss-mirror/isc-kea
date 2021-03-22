3f485f4a1f5f5b137b3d6eb8747675e1c1b6d579
Razvan Becheriu <razvan@isc.org>
2021-03-22 20:36:22 +0200

    [#1763] compute minimum set of dependencies

--- tools/update_makefiles.sh
+++ tools/update_makefiles.sh
@@ -37,9 +37,9 @@ extract_includes() {
 	INCLUDES_LIST=$(echo ${INCLUDES_LIST} | tr -s " ")
 	# order dependencies in the order of compilation
 	FILTERED_INCLUDES_LIST=
-	for i in ${INCLUDES_LIST}; do
-		if test $(echo "${LIBRARIES_LIST}" | grep "${i}" | wc -l) -ne 0; then
-			FILTERED_INCLUDES_LIST="${FILTERED_INCLUDES_LIST} ${i}"
+	for i in ${LIBRARIES_LIST}; do
+		if test $(echo "${INCLUDES_LIST}" | grep "\b${i}\b" | wc -l) -ne 0; then
+			FILTERED_INCLUDES_LIST="${i} ${FILTERED_INCLUDES_LIST}"
 		fi
 	done
 	echo "${FILTERED_INCLUDES_LIST}"
@@ -62,7 +62,7 @@ extract_non_include_files() {
 	RAW_INCLUDES_LIST=$(echo "${SEARCH_FILES}" | xargs grep "^#include " 2>/dev/null)
 	# filter only included headers found in other libraries by using the form 'other_lib_name/header_file.h'
 	# to do this it is required to select the string between '<' and '>', searching for '/' character, search for the extension marker '.' and returning the name after last '/'
-	RAW_INCLUDES_LIST=$(echo "${RAW_INCLUDES_LIST}" | cut -d "#" -f 2 | tr "\"" " " | cut -d "<" -f 2 | cut -d ">" -f 1 | grep "\/" | grep "${2}" | cut -d "/" -f 2 | grep "\." | sort | uniq)
+	RAW_INCLUDES_LIST=$(echo "${RAW_INCLUDES_LIST}" | cut -d "#" -f 2 | tr "\"" " " | cut -d "<" -f 2 | cut -d ">" -f 1 | grep "\/" | grep "\b${2}\b" | cut -d "/" -f 2 | grep "\." | sort | uniq)
 	# select only files in dependency library folder and strip full path
 	RELATIVE_SEARCH_FILES=$(echo "${FILE_LIST}" | grep "${EXTERNAL_FOLDER}" | sed -e "s#${REPO_FOLDER}${EXTERNAL_FOLDER}##g")
 	# search for the header file but also for source files
@@ -96,6 +96,20 @@ extract_dependencies() {
 	echo "${1}" | grep -Eo "\b${2}\b.*$"
 }
 
+# extract computed dependency for specified library
+#
+# param ${1} library name for which the dependency list is retrieved
+# return stored value of computed dependencies or 'NONE' if dependencies have not been computed yet
+extract_computed_dependencies() {
+	NAME="COMPUTED_DEPENDENCIES_${1}"
+	if test -n "${!NAME+x}"; then
+		echo "${!NAME}"
+	else
+		echo "NONE"
+	fi
+}
+
+# if wrong number of parameters print usage
 if test ${#} -ne 1; then
 	echo "Usage: ${0} path/to/kea/repo"
 	exit
@@ -107,7 +121,8 @@ REPO_FOLDER=${1}
 # filter all Makefile.am files
 MAKEFILES_LIST=$(find ${REPO_FOLDER} | grep "Makefile\.am" | grep "src\/" | sort)
 
-if test -z ${MAKEFILES_LIST}; then
+# if no Makefile.am found exit
+if test -z "${MAKEFILES_LIST}"; then
 	echo "invalid repo path: no Makefile.am file found"
 	exit
 fi
@@ -118,6 +133,7 @@ echo "${MAKEFILES_LIST}"
 # base Makefile.am for all sources is in src/lib/Makefile.am
 BASE_MAKEFILE=$(echo "${MAKEFILES_LIST}" | grep "src\/lib\/Makefile.am")
 
+# if no src/lib/Makefile.am found exit
 if test -z ${BASE_MAKEFILE}; then
 	echo "invalid repo path: no src/lib/Makefile.am file found"
 	exit
@@ -184,7 +200,10 @@ for i in ${LIBRARIES_LIST}; do
 				echo "only header files: ${EXTERNAL_HEADERS}"
 			fi
 		else
-			NON_RECURSIVE_BASE_DEPENDENCIES="${NON_RECURSIVE_BASE_DEPENDENCIES} ${j}"
+			# don't add current library to it's dependencies list
+			if test ${j} != ${i}; then
+				NON_RECURSIVE_BASE_DEPENDENCIES="${NON_RECURSIVE_BASE_DEPENDENCIES} ${j}"
+			fi
 		fi
 	done
 	# all found dependencies in the reverse compilation order
@@ -198,13 +217,27 @@ for i in ${LIBRARIES_LIST}; do
 	# all valid dependencies that can be added by each dependency library
 	DEPENDENCIES=
 	for j in ${NON_RECURSIVE_BASE_DEPENDENCIES}; do
-		NEW_DEPENDENCIES=$(extract_dependencies "${REVERSE_LIBRARIES_LIST}" "${j}")
-		if test $(echo ${NEW_DEPENDENCIES} | wc -w) -gt $(echo ${DEPENDENCIES} | wc -w); then
-			DEPENDENCIES=${NEW_DEPENDENCIES}
+		NEW_DEPENDENCIES=$(extract_computed_dependencies "${j}")
+		if test "${NEW_DEPENDENCIES}" == "NONE"; then
+			echo "### ERROR ### computed dependency not found for ${j}"
+		else
+			DEPENDENCIES="${NEW_DEPENDENCIES} ${DEPENDENCIES}"
 		fi
 	done
+	DEPENDENCIES=$(echo "${DEPENDENCIES} ${NON_RECURSIVE_BASE_DEPENDENCIES}" | tr -s " " "\n" | sort | uniq | xargs)
 	echo "${i} dependencies:"
 	echo "${DEPENDENCIES}"
+	# order dependencies in the order of compilation
+	SORTED_DEPENDENCIES=
+	for j in ${LIBRARIES_LIST}; do
+		if test $(echo "${DEPENDENCIES}" | grep "\b${j}\b" | wc -l) -ne 0; then
+			SORTED_DEPENDENCIES="${j} ${SORTED_DEPENDENCIES}"
+		fi
+	done
+	echo "${i} sorted dependencies:"
+	echo "${SORTED_DEPENDENCIES}"
+	declare COMPUTED_DEPENDENCIES_${i}="${SORTED_DEPENDENCIES}"
+
 	# todo: only extent with dependencies that each dependency asks for, not all possible dependencies
 done
 
